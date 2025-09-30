6eb7b8a55603e5179cd6f5750b60c9275ab43cd0
Viacheslav Rukavichkin <s@u-wr.ru>
2025-09-30 18:01:18 +0300

    Add filter block statuses

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,41 +1,18 @@
 import React, { useState, useEffect } from 'react';
 import apiClient from '../api/axiosConfig';
 import { getUserDataFromToken } from '../utils/authUtils';
-import { Button } from 'react-bootstrap';
 import TaskList from '../Components/TaskList';
-import TaskFormModal from '../Components/TaskFormModal';
+// import { Button } from 'react-bootstrap';
 
 const HomePage = () => {
   const [tasks, setTasks] = useState([]);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
 
-  const [showCreateModal, setShowCreateModal] = useState(false);
-  const [newTask, setNewTask] = useState({
-    title: '',
-    description: '',
-    // client: '',
-    // status: '',
-    // worker: '',
-    // start_time: '',
-    // end_time: '',
-  });
-
-  const [showEditModal, setShowEditModal] = useState(false);
-  const [editTask, setEditTask] = useState({
-    title: '',
-    description: '',
-    // client: '',
-    // status: '',
-    // worker: '',
-    // start_time: '',
-    // end_time: '',
-  });
-  const [editTaskId, setEditTaskId] = useState(null);
-
   const [clients, setClients] = useState([]);
   const [statuses, setStatuses] = useState([]);
   const [employees, setEmployees] = useState([]);
+  const [statusFilter, setStatusFilter] = useState([]); // Изначально пустой массив, что означает "все"
 
   const userData = getUserDataFromToken();
   const isRole = {
@@ -72,43 +49,34 @@ const HomePage = () => {
     }
   }, [isRole.client, isRole.admin, isRole.superAdmin]);
 
-  const handleCreateChange = (e) => setNewTask({ ...newTask, [e.target.name]: e.target.value });
+  const handleFilter = (clickedStatusId) => {
+    let newFilter;
 
-  const handleCreateSave = async () => {
-    try {
-      const response = await apiClient.post('/tasks', newTask, { headers: { 'Content-Type': 'application/ld+json' } });
-      setTasks([...tasks, response.data]);
-      setShowCreateModal(false);
-    } catch (err) {
-      setError('Ошибка при создании задачи.');
+    if (clickedStatusId === 'all') {
+      newFilter = []; // Если нажата кнопка "Все", очищаем все фильтры
+    } else {
+      // Проверяем, есть ли уже этот статус в фильтре
+      if (statusFilter.includes(clickedStatusId)) {
+        // Если есть, удаляем его (отключаем фильтр)
+        newFilter = statusFilter.filter(s => s !== clickedStatusId);
+      } else {
+        // Если нет, добавляем его (включаем фильтр)
+        newFilter = [...statusFilter, clickedStatusId];
+      }
     }
-  };
 
-  const handleEdit = (task) => {
-    setEditTask({
-      title: task.title,
-      description: task.description,
-      client: task.client || '',
-      status: task.status || '',
-      worker: task.worker || '',
-      // start_time: task.start_time || '',
-      // end_time: task.end_time || '',
-    });
-    setEditTaskId(task.id);
-    setShowEditModal(true);
-  };
+    setStatusFilter(newFilter); // Обновляем состояние фильтра
 
-  const handleEditChange = (e) => setEditTask({ ...editTask, [e.target.name]: e.target.value });
-
-  const handleEditSave = async () => {
-    try {
-      // console.log(editTask);
-      const response = await apiClient.patch(`/tasks/${editTaskId}`, editTask, { headers: { 'Content-Type': 'application/merge-patch+json' } });
-      setTasks(tasks.map(t => t.id === editTaskId ? response.data : t));
-      setShowEditModal(false);
-    } catch (err) {
-      setError('Ошибка при обновлении задачи.');
+    // Формируем URL для запроса задач
+    let apiUrl = '/tasks';
+    if (newFilter.length > 0) {
+      // Если есть выбранные статусы, добавляем их в параметры
+      apiUrl += `?statuses=${newFilter.join(',')}`;
     }
+
+    apiClient.get(apiUrl)
+      .then(response => setTasks(response.data['member'] || []))
+      .catch(err => setError('Не удалось загрузить список задач.'));
   };
 
   const handleDelete = async (id) => {
@@ -126,39 +94,40 @@ const HomePage = () => {
   if (error) return <div className="alert alert-danger">{error}</div>;
 
   return (
-    <div>
-      <h2>Список задач</h2>
-      <Button variant="primary" onClick={() => setShowCreateModal(true)}>Создать задачу</Button>
+    <div className='task-list'>
+      <div className="py-2 px-2 px-md-4 px-lg-5 d-flex justify-content-between align-items-center task_list_header">
+        <h2>Задачи</h2>
+        <div className="task-list-filter">
+          <div className="filter-statuses">
+            <div className='list-unstyled filter-statuses-list'>
+              {/* Кнопки статусов */}
+              {statuses.map(status => (
+                <button
+                  className={`btn-switch${statusFilter.includes(status.id) ? ' active' : ''}`}
+                  id={status.id}
+                  key={status.id}
+                  onClick={() => handleFilter(status.id)}
+                >
+                  {status.status}
+                </button>
+              ))}
+              <button className={`btn-switch${statusFilter.length === 0 ? ' active' : ''}`} onClick={() => handleFilter('all')}>Все</button>
+            </div>
+          </div>
+          <div className="filter-block">
+            <div className="filter-create">create task</div>
+            <div className="filter-worker">Worker</div>
+          </div>
+        </div>
+      </div>
       <TaskList 
         tasks={tasks} 
         clients={clients} 
         statuses={statuses} 
         employees={employees} 
         isRole={isRole} 
-        onEdit={handleEdit} 
+        // onEdit={handleEdit} 
         onDelete={handleDelete} 
-      />
-      <TaskFormModal 
-        show={showCreateModal} 
-        onHide={() => setShowCreateModal(false)} 
-        taskData={newTask} 
-        onChange={handleCreateChange} 
-        onSave={handleCreateSave} 
-        clients={clients} 
-        statuses={statuses} 
-        employees={employees} 
-        isRole={isRole}
-      />
-      <TaskFormModal 
-        show={showEditModal} 
-        onHide={() => setShowEditModal(false)} 
-        taskData={editTask} 
-        onChange={handleEditChange} 
-        onSave={handleEditSave} 
-        clients={clients} 
-        statuses={statuses} 
-        employees={employees}
-        isRole={isRole}
       />
     </div>
   );
--- src/Pages/NewTaskPage.jsx
+++ src/Pages/NewTaskPage.jsx
@@ -1,9 +1,87 @@
-// import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import { jwtDecode } from 'jwt-decode'; // Импортируем декодер
 import { Navigate } from 'react-router-dom';
+import TaskFormModal from '../Components/TaskFormModal';
+import { Button } from 'react-bootstrap';
+import { getUserDataFromToken } from '../utils/authUtils';
 
 const NewTaskPage = () => {
     const token = localStorage.getItem('authToken');
+
+    //-->
+    const [showCreateModal, setShowCreateModal] = useState(false);
+    const [clients, setClients] = useState([]);
+    const [statuses, setStatuses] = useState([]);
+    const [employees, setEmployees] = useState([]);
+    const userData = getUserDataFromToken();
+    const isRole = {
+        client: userData?.roles.includes('ROLE_USER') && !userData?.roles.includes('ROLE_ADMIN') && !userData?.roles.includes('ROLE_SUPER_ADMIN'),
+        admin: userData?.roles.includes('ROLE_ADMIN') && !userData?.roles.includes('ROLE_SUPER_ADMIN'),
+        superAdmin: userData?.roles.includes('ROLE_SUPER_ADMIN'),
+    };
+
+    const [newTask, setNewTask] = useState({
+        title: '',
+        description: '',
+        // client: '',
+        // status: '',
+        // worker: '',
+        // start_time: '',
+        // end_time: '',
+    });
+
+    const [showEditModal, setShowEditModal] = useState(false);
+    const [editTask, setEditTask] = useState({
+        title: '',
+        description: '',
+        // client: '',
+        // status: '',
+        // worker: '',
+        // start_time: '',
+        // end_time: '',
+    });
+    const [editTaskId, setEditTaskId] = useState(null);
+
+    const handleCreateChange = (e) => setNewTask({ ...newTask, [e.target.name]: e.target.value });
+
+    const handleCreateSave = async () => {
+        try {
+        const response = await apiClient.post('/tasks', newTask, { headers: { 'Content-Type': 'application/ld+json' } });
+        setTasks([...tasks, response.data]);
+        setShowCreateModal(false);
+        } catch (err) {
+        setError('Ошибка при создании задачи.');
+        }
+    };
+
+    const handleEdit = (task) => {
+        setEditTask({
+        title: task.title,
+        description: task.description,
+        client: task.client || '',
+        status: task.status || '',
+        worker: task.worker || '',
+        // start_time: task.start_time || '',
+        // end_time: task.end_time || '',
+        });
+        setEditTaskId(task.id);
+        setShowEditModal(true);
+    };
+
+    const handleEditChange = (e) => setEditTask({ ...editTask, [e.target.name]: e.target.value });
+
+    const handleEditSave = async () => {
+        try {
+        // console.log(editTask);
+        const response = await apiClient.patch(`/tasks/${editTaskId}`, editTask, { headers: { 'Content-Type': 'application/merge-patch+json' } });
+        setTasks(tasks.map(t => t.id === editTaskId ? response.data : t));
+        setShowEditModal(false);
+        } catch (err) {
+        setError('Ошибка при обновлении задачи.');
+        }
+    };
+
+    //-->
 
     if (!token) {
         // Если токена нет, перенаправляем на страницу входа
@@ -13,6 +91,29 @@ const NewTaskPage = () => {
     return (
         <div className="cont">
             <h2 className="header">Страница создания новой задачи</h2>
+            <Button variant="primary" onClick={() => setShowCreateModal(true)}>Создать задачу</Button>
+            <TaskFormModal 
+                show={showCreateModal} 
+                onHide={() => setShowCreateModal(false)} 
+                taskData={newTask} 
+                onChange={handleCreateChange} 
+                onSave={handleCreateSave} 
+                clients={clients} 
+                statuses={statuses} 
+                employees={employees} 
+                isRole={isRole}
+            />
+            <TaskFormModal 
+                show={showEditModal} 
+                onHide={() => setShowEditModal(false)} 
+                taskData={editTask} 
+                onChange={handleEditChange} 
+                onSave={handleEditSave} 
+                clients={clients} 
+                statuses={statuses} 
+                employees={employees}
+                isRole={isRole}
+            />
         </div>
     );
 };
