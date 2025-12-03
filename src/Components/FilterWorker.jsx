a19be89753988cc586b4bea8e20d7c8a112eed50
Viacheslav Rukavichkin <s@u-wr.ru>
2025-12-03 12:42:28 +0300

    Fix WilterWorker - add posibility choise workers

--- src/Components/FilterWorker.jsx
+++ src/Components/FilterWorker.jsx
@@ -2,51 +2,54 @@ import UserSVG from '../Components/UserSVG';
 
 
 const FilterWorker = ({ className = "filter-worker", employees, isRole, workerFilter, setWorkerFilter }) => {
+    if (isRole.superAdmin) {
+        className = `${className} d-flex justify-content-between align-items-center`
+    }
     return (
         <div className={className}>
             {(isRole.client) && (
-            <div className="filter-worker-wrap">
-                <div className="filter-worker-label">Исполнитель:</div>
-                <div className="filter-worker-btns">
-                <button
-                    className={`btn-switch users${workerFilter === 1 ? ' active' : ''}`}
-                    key="1"
-                    onClick={() => setWorkerFilter(1)}
-                >
-                    <UserSVG type="1" />
-                </button>
-                <button
-                    className={`btn-switch users${workerFilter === 2 ? ' active' : ''}`}
-                    key="2"
-                    onClick={() => setWorkerFilter(2)}
-                >
-                    <UserSVG type="0" />
-                </button>
-                <button
-                    className={`btn-switch users${workerFilter === '' ? ' active' : ''}`}
-                    key="3"
-                    onClick={() => setWorkerFilter("")}
-                >
-                    <UserSVG type="all" />
-                </button>
+                <div className="filter-worker-wrap">
+                    <div className="filter-worker-label">Исполнитель:</div>
+                    <div className="filter-worker-btns">
+                    <button
+                        className={`btn-switch users${workerFilter === 1 ? ' active' : ''}`}
+                        key="1"
+                        onClick={() => setWorkerFilter(1)}
+                    >
+                        <UserSVG type="1" />
+                    </button>
+                    <button
+                        className={`btn-switch users${workerFilter === 2 ? ' active' : ''}`}
+                        key="2"
+                        onClick={() => setWorkerFilter(2)}
+                    >
+                        <UserSVG type="0" />
+                    </button>
+                    <button
+                        className={`btn-switch users${workerFilter === '' ? ' active' : ''}`}
+                        key="3"
+                        onClick={() => setWorkerFilter("")}
+                    >
+                        <UserSVG type="all" />
+                    </button>
+                    </div>
                 </div>
-            </div>
             )}
             {(isRole.superAdmin) && (
-            <>
-                <label htmlFor="worker-filter" className="form-label mb-0">Исполнитель:</label>
-                <select
-                id="worker-filter"
-                className="form-select"
-                value={workerFilter}
-                onChange={(e) => setWorkerFilter(e.target.value)}
-                >
-                <option value="">Все</option>
-                {employees.map(employee => (
-                    <option key={employee.id} value={employee.id}>{employee.user_id.name} {employee.user_id.surname}</option>
-                ))}
-                </select>
-            </>
+                <>
+                    <label htmlFor="worker-filter" className="form-label mb-0">Исполнитель:</label>
+                    <select
+                    id="worker-filter"
+                    className="form-select"
+                    value={workerFilter}
+                    onChange={(e) => setWorkerFilter(e.target.value)}
+                    >
+                        <option value="">Все</option>
+                        {employees.map(employee => (
+                            <option key={employee.id} value={employee.id}>{employee.user_id.name} {employee.user_id.surname}</option>
+                        ))}
+                    </select>
+                </>
             )}
         </div>
     );
